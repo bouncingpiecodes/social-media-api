3b86f25972bf183e12762a579133aa7a6eef0af3
“kara <bouncingpie@icloud.com>
2023-02-06 15:54:23 -0500

    const objects

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,11 @@
-const router = require("express").Router;
+const router = require("express").Router();
+
+const {
+  getThought,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
